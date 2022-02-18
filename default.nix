d00236206f9b2afc69ee01ab4f66d969f52375e1
Rahul Butani <rrbutani@users.noreply.github.com>
2022-02-17 23:04:11 -0600

    prebuild: more plans

--- default.nix
+++ default.nix
@@ -34,7 +34,51 @@ let
   #
   # Produces a tarball (has the different systems, etc.).
   preBuildDerivations = { derivations }:
-    # Assert that all the derivations are for different systems?
+    # Assert that all the derivations are for different systems (i.e. only one derivation per system)
 
+    # Produces a tarball with:
+    # ```
+    # |- <system name>
+    # |  |- inputs.json (details about the transitive closure of the derivation)
+    # |  |- outputs
+    # |     |- lib
+    # |     |- bin
+    # |     |- doc
+    # |     |- ...
+    # |- ...
+    # ```
+
+    # TODO: not sure what the best way to get `inputs.json` is; perhaps `exportReferencesGraph`?
+
+    # TODO: can we just use the NAR for the derivation's store path somehow?
+
+    ;
 
   # TODO: strict/lenient mode on the "replace" step (i.e. warn on different inputs for the runtime version or error)
+
+
+  # Takes a derivation and a tarball.
+  #
+  # Gives back a new derivation that can be used in lieu of the original but uses the prebuilt artifacts from the
+  # tarball.
+  substituteForPrebuilt = { original, tarball, errorOnPrebuiltMismatch ? false }:
+    # Assert that original is a derivation and that `tarball` is a path/derivation.
+    #
+    # Produce a new derivation that has the same outs but extracts them from the tarball.
+    # Have the derivation steal test/check phases? (or just override the fetch/configure/build phases, etc.)
+    #
+    # Upfront check that the derivation's closure etc matches; warn if not, error if the flag above tells us
+    # to.
+
+    # TODO: should we have this set `outputHash`? might be tricky to get `preBuild` to spit these out (a post processing step
+    # that emits a nix file that we can then import here?)
+    #
+    # maybe make it an optional thing?
+    #
+    # or, don't take a tarball but a path that we'll append the package name + "-hash.nix"/".tar.gz" to
+    #
+    # and have preBuildDerivations spit this directory out
+  ;
+in {
+  inherit preBuildNixpkg preBuildDerivations substituteForPrebuilt;
+}
