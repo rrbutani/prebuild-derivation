e38c806bb6ad6fd5ba6f2fde33f650b8076523f4
Rahul Butani <rrbutani@users.noreply.github.com>
2022-03-07 08:46:11 -0600

    prebuild: support multi-output derivations

--- flake-test/flake.nix
+++ flake-test/flake.nix
@@ -31,33 +31,70 @@
       , outputsToPackage ? null
       }:
     let
-      np = import nixpkgs { system = "aarch64-darwin"; };
-      target = np.nix;
+      targetOutputList = if outputsToPackage == null
+        then if target ? outputs then target.outputs else [ target.outputName ]
+        else outputsToPackage;
 
-      # useRecursiveNix = true;
-
-      # targetDrvPath = builtins.unsafeDiscardStringContext (toString target.drvPath);
+      # (this is really a job for filterAttrs from nixpkgs.lib)
+      targetOutputs = builtins.listToAttrs (
+        builtins.map (
+          output: { name = output; value = target.${output}; }
+        ) targetOutputList
+      );
     in
-      derivation {
-        name = "test";
-        system = "aarch64-darwin";
-        builder = "${np.bash}/bin/bash";
+    derivation {
+      name = if target ? pname && target ? version then
+        target.pname + "-prebuilt-" + target.version
+      else
+        target.name + "-prebuilt";
 
-        exportReferencesGraph.target = [ target ];
-        __structuredAttrs = true;
+      inherit system;
+      builder = "${np.bash}/bin/bash";
 
-        script = ./metadata.py;
-        deps = with np; [ nix_2_4 coreutils jq python310 gnutar xz ];
-        targetPath = target;
-        args = [(
+      exportReferencesGraph.target = [ target ];
+      __structuredAttrs = true;
 
-          # prebuild:
-          # store runtime deps (from exportReferencesGraph)
-          # store build deps (from `show-derivation` on the target)
-          # store the actual thing (nix dump-path -- target, piped into xz)
-          #   - compute the checksum and store it?
+      deps = with np; [ nix_2_4 coreutils jq python310 gnutar xz ];
+      targetPath = target.all;
+      inherit targetOutputs;
+      args = [(
+        np.writeScript "prebuild.sh" ''
+          . "''${NIX_ATTRS_SH_FILE}"
+          for d in "''${deps[@]}"; do export PATH="''${PATH}:''${d}/bin"; done
 
-          # ~secret~ string context functions: https://github.com/NixOS/nix/commit/1d757292d0cb78beec32fcdfe15c2944a4bc4a95#diff-f9b278c45a70ce046ba391eaca009baf0f306a99c2328e1a651bbb36cf22d802
+          getAttr() { jq -r "''${@}" <"''${NIX_ATTRS_JSON_FILE}"; }
+
+          out="''${outputs[out]}";
+          mkdir -p $out
+
+          nix show-derivation ''${targetPath} > drv.json
+          getAttr ".target | .[] | select(.path == \"$targetPath\")" > runtime.json
+
+          python3 "${./grab_metadata.py}" \
+              runtime.json drv.json \
+            | jq > $out/metadata.json
+
+          for output_name in "''${!targetOutputs[@]}"; do
+            echo $output_name ''${targetOutputs[$output_name]}
+            tar rf $out/archive.tar -C "''${targetOutputs[$output_name]}" . --transform "s,^,''${output_name}/,"
+          done
+
+          xz $out/archive.tar
+        ''
+      )];
+    };
+
+    restore =
+      { dir
+      , sourceDerivation
+      , checkRuntimeDeps ? true
+      , checkBuildDeps ? false
+      , checkExtraAttrs ? false
+      , useContentAddressedDerivations ? true
+      }:
+    let
+      archiveFile = "${dir}/archive.tar.xz";
+      metadataFile = "${dir}/metadata.json";
 
           # substitute:
           # compute deps for original (nix show-derivation on the drvPath – passed in with unsafeDiscardStringContext)
