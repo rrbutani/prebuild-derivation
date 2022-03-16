5ec5929703a21a271e831f9dd2cf46f5d7539809
Rahul Butani <rrbutani@users.noreply.github.com>
2022-03-15 19:38:56 -0500

    tests: add an e2e test that uses the example
    lots of fun IFD and flake shenanigans here

--- flake.nix
+++ flake.nix
@@ -47,12 +47,54 @@
         regularChecks = mapTests' (n: "same${n}") check;
         caInputChecks = mapTests' (n: "same${n}CA") checkCA;
 
+        e2e = let
+          # exampleFlake = builtins.getFlake (toString ./example);
+          exampleFlake = (import ./example/flake.nix).outputs { self = exampleFlake; inherit flake-utils nixpkgs; pbd = self; };
+          flakeWithPrebuilts = exampleFlake.packages.${system}.withPrebuilts;
+          # flakeWithPrebuilts' = builtins.getFlake (toString flakeWithPrebuilts);
+
+          # This is IFD and, because `nix flake check` evaluates all systems, thus breaks if your
+          # system cannot build for *all* the defaultSystems.
+          #
+          # So, we wrap it in a tryEval. edit: nvm, this doesn't help...
+          #
+          # Gonna have to resort to hardcoding a system (yuck). TODO: find a workaround?
+          # flakeWithPrebuilts' = builtins.tryEval (
+          #   (import flakeWithPrebuilts).outputs { self = flakeWithPrebuilts'; inherit flake-utils nixpkgs; pbd = self; }
+          # );
+          #
+          # Okay: the workaround is that this test only runs with `--impure` (even though it is actually perfectly
+          # pure).
+          flakeWithPrebuiltsTarball = "${flakeWithPrebuilts}";
+          flakeWithPrebuiltsExtracted = derivation {
+            name = "extract-flake-tarball";
+            inherit system;
+            builder = "${nixpkgs'.bash}/bin/bash";
+            args = [(nixpkgs'.writeScript "extract.sh" ''
+              export PATH="${nixpkgs'.xz}/bin"
+              ${nixpkgs'.coreutils}/bin/mkdir -p $out
+              ${nixpkgs'.gnutar}/bin/tar xvf "${flakeWithPrebuiltsTarball}/archive.tar.xz" -C $out
+            '')];
+          };
+
+          flakeWithPrebuilts' =
+            (import "${flakeWithPrebuiltsExtracted}/tarball-with-prebuilts/flake.nix").outputs { self = flakeWithPrebuilts'; inherit flake-utils nixpkgs; pbd = self; }
+          ;
+
+          dbg = x: builtins.trace x x;
+        in
+          if builtins ? currentSystem && system == builtins.currentSystem
+          then
+            dbg flakeWithPrebuilts'.checks.${system}
+          else
+            {};
+
         # TODO: check that the pre-built derivation does *not* have a dep on the original package.
         # (until we fix #2 – self-references – we will fail this kind of check).
-      in regularChecks // caInputChecks;
+      in regularChecks // caInputChecks // e2e;
   });
 
   # TODO: tests
   # TODO: examples
-  # TODO: CI
+  # TODO: CI (`nix flake check`, `nix flake check --impure`)
 }
